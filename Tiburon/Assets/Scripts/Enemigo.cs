8712c1ff47735ba2e23b96413cbb62a784ebcd28
Arpad Kiss <kissarpico22@gmail.com>
2024-06-21 22:24:20 +0200

    fin

--- Tiburon/Assets/Scripts/Enemigo.cs
+++ Tiburon/Assets/Scripts/Enemigo.cs
@@ -3,38 +3,30 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
-    public GameObject enemyPrefab; // Prefab del enemigo
-    public float xPosition = 0; // Posición en el eje X donde se crearán los enemigos
-    public int maxEnemies = 10; // Máximo número de enemigos a crear
-    public float minSpawnTime = 1f; // Tiempo mínimo entre spawns
-    public float maxSpawnTime = 5f; // Tiempo máximo entre spawns
+    [SerializeField] private GameObject[] spawns;
+    [SerializeField] private GameObject enemyPre;
+    [SerializeField] private int maxEne = 209;
+    
+    private Transform player; // Referencia al jugador
 
     void Start()
     {
-        StartCoroutine(SpawnEnemies()); // Iniciamos la coroutinea
+        player = GameObject.FindGameObjectWithTag("Player").transform; // Buscamos al jugador por su tag
+
+        StartCoroutine(SpawnCorrutine());
     }
 
-    IEnumerator SpawnEnemies()
+    private IEnumerator SpawnCorrutine()
     {
-        GameObject emptyObject = GameObject.Find("Spawn"); // Encuentra el objeto vacío en la escena
-        float yPosition = emptyObject.transform.position.y; // Obtiene la posición en el eje Y del objeto vacío
-
-        float[] zPositions = new float[] { 6, 3, 0}; // Posiciones en el eje Z donde se crearán los enemigos
-
-        for (int i = 0; i < maxEnemies; i++) // Para cada enemigo hasta maxEnemies
+        yield return new WaitForSeconds(3);
+        for (int i = 0; i < maxEne; i++)
         {
-            float zPosition =
-                zPositions
-                    [Random.Range(0, zPositions.Length)]; // Escogemos una posición en el eje Z de manera aleatoria
-            Vector3
-                enemyPosition =
-                    new Vector3(transform.position.x, yPosition,
-                        zPosition); // Creamos un vector de posición para el enemigo manteniendo la posición en X del objeto que crea los enemigos y la posición en Y del objeto vacío
-            Instantiate(enemyPrefab, enemyPosition, Quaternion.identity); // Creamos el enemigo
-
-            float spawnTime = Random.Range(minSpawnTime, maxSpawnTime); // Calculamos un tiempo de spawn aleatorio
-            yield return
-                new WaitForSeconds(spawnTime); // Esperamos el tiempo de spawn antes de crear el próximo enemigo
+            SpawnEne();
+            yield return new WaitForSeconds(1);
         }
     }
+    
+    private void SpawnEne() => Instantiate(enemyPre, spawns[Random.Range(0, spawns.Length)].transform.position, Quaternion.identity);
+
+   
 }
